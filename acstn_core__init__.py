083069959e75fc752d30e04de224f93e9f00e27f
thom899g <153515950+thom899g@users.noreply.github.com>
2026-02-25 15:19:48 +0100

    Automated commit by HiveMind

---
+++ acstn_core__init__.py
@@ -0,0 +1,8 @@
+"""
+Autonomous Cross-Domain Scalable Trading Network (ACSTN) Core
+Version: 1.0.0
+Mission-Critical Trading Infrastructure
+"""
+
+__version__ = "1.0.0"
+__author__ = "ACSTN Architecture Team"
